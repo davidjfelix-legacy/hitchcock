429aa5fd90bdc621003a0b6d57c723f08a04c1cf
David J Felix <felix.davidj@gmail.com>
2016-09-25 21:58:16 -0400

    Work on json views for data endpoints

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -2,14 +2,10 @@ defmodule Hitchcock.UserView do
   use Hitchcock.Web, :view
 
   def render("index.json", %{users: users}) do
-    %{data: render_many(users, Hitchcock.UserView, "user.json")}
+    render_many(users, Hitchcock.UserView, "user.json")
   end
 
   def render("show.json", %{user: user}) do
-    %{data: render_one(user, Hitchcock.UserView, "user.json")}
-  end
-
-  def render("user.json", %{user: user}) do
     %{id: user.id}
   end
 end
--- web/views/video_view.ex
+++ web/views/video_view.ex
@@ -2,14 +2,10 @@ defmodule Hitchcock.VideoView do
   use Hitchcock.Web, :view
 
   def render("index.json", %{videos: videos}) do
-    %{data: render_many(videos, Hitchcock.VideoView, "video.json")}
+    render_many(videos, Hitchcock.VideoView, "video.json")
   end
 
   def render("show.json", %{video: video}) do
-    %{data: render_one(video, Hitchcock.VideoView, "video.json")}
-  end
-
-  def render("video.json", %{video: video}) do
     %{id: video.id,
       title: video.title}
   end
